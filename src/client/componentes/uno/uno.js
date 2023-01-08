74d85bd548b1ced03a83d824104338918c53e956
Juan Luis <juanluisclaure24@live.com>
2023-01-08 13:52:31 -0400

    Update:img
    esta imagen remplaza un textoyy se vuelve en gif pra dar apariencia y demas blablablalba

--- src/client/componentes/uno/uno.js
+++
@@ -1,3 +0,0 @@
-const riot = require('riot')
-riot.tag2('componente-uno', '<figure> <figcaption> <b> Just css waoo </b></figcaption> <pre class="prettyprint lang-css">\n\n  class Voila {public:     // Voila     static const string VOILA = ⁗Voila⁗;     // will not interfere with embedded <a href=⁗#voila2⁗>tags</a>.}\n\n</pre> </figure> <br>', 'componente-uno .algo,[data-is="componente-uno"] .algo{ background-color:red; }', '', function(opts) {
-});
